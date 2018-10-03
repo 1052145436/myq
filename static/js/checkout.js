5096e0ed7f30c2d1612dbac4094517ea5cda17d0
1052145436@qq.com <1052145436@qq.com>
2018-10-03 11:34:50 +0800

    【提交】单页逻辑修改。

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -10,6 +10,21 @@ var checkoutObj = {
             // 函数功能定义
             var commonObj = {
                 initUI:function(){
+                    // 所购买的商品列表
+                    var isShowGoodsList = false;
+                    $('#product-list-title-bar').click(function(){
+                        isShowGoodsList = !isShowGoodsList;
+                        if(isShowGoodsList == true){
+                            $('#product-list-ul').stop().slideDown();
+                            $('#showOrNotBtn>span').html('收缩');
+                            $('#showOrNotBtn>i').addClass('rotateIcon');
+                        }else{
+                            $('#product-list-ul').stop().slideUp();
+                            $('#showOrNotBtn>span').html('展开');
+                            $('#showOrNotBtn>i').removeClass('rotateIcon');
+                        }
+                    });
+
                     // 添加更多商品
                     $('#coupon-tip').click(function(){
                         $('#add-more-goods').show();
@@ -17,7 +32,7 @@ var checkoutObj = {
                             'overflow-y':'hidden'
                         });
                     });
-                    $('#add-more-goods-closeBtn').click(function(){
+                    $('.add-more-goods-closeBtn').click(function(){
                         $('#add-more-goods').hide();
                         $('body').css({
                             'overflow-y':'auto'
