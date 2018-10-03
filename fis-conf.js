9df0215119b9dcf2c0a4cc43eb15c82b4d1eb3ef
1052145436@qq.com <1052145436@qq.com>
2018-10-03 10:01:59 +0800

    【提交】复合单页逻辑修改。

--- fis-conf.js
+++ fis-conf.js
@@ -1,83 +1,49 @@
-fis
-    .set('date', new Date)
-    .set('project.watch.usePolling', true)
-
-    .match('/static/images/**/*.{png,jpg}', {
-      useHash: false,
-      release:'/$0',
-      query: '?t=' + (fis.get('date').getYear() + 1900)
-      + (fis.get('date').getMonth() + 1)
-      + (fis.get('date').getDate())
-    })
-
-    .match('*.{js,css,scss}', {
-      useHash: true
-    })
-    .match('/static/scss/(*).{css,scss}', {
-      parser: fis.plugin('node-sass'),
-      rExt: '.css',
-      postprocessor: fis.plugin('autoprefixer', {
-          browsers: [
-              'last 3 versions',
-              'ie >= 10',
-              'ie_mob >= 10',
-              'ff >= 30',
-              'chrome >= 34',
-              'safari >= 6',
-              'opera >= 12.1',
-              'ios >= 6',
-              'android >= 4.4',
-              'bb >= 10',
-              'and_uc 9.9'
-          ],
-          cascade: true
-      }),
-      release: '/static/css/$1'
-    })
-
-    .match('/static/js/*.js', {
-      useHash: true,
-      isMod: true,
-      release: '/$0'
-    })
-    .match('/static/plugins/*.js', {
-      release: '/$0'
-    })
-
-    .hook('commonjs')
-    .match('::package', {
-      postpackager: fis.plugin('loader', {
-        resourceType: 'mod',
-        useInlineMap: true,
-        allInOne: {
-          includeAsyncs: true,
-          sourceMap: true,
-          useTrack: false
-        }
-      }),
-      packager: fis.plugin('map')
-    })
-
-
-  // 生产
-    .media('prod')
-
-    .match('*.png',{
-      optimizer:fis.plugin('png-compressor')
-    })
-
-    .match('*.{css,scss}',{
-      useSprite: true,
-      optimizer: fis.plugin('clean-css')
-    })
-
-    .match('/static/js/*.js',{
-      optimizer: fis.plugin('uglify-js')
-    })
-
-    .match('::package', {
-      spriter: fis.plugin('csssprites')
-    })
+fis.hook('relative')
     .match('**',{
-      domain:'https://xn.15166.com/m'
+        relative:true
+    })
+    .match('*.less',{
+        parser:fis.plugin('less'),
+        rExt:'.css',
+        useHash:true
+    })
+    .match('*.scss', {
+        parser: fis.plugin('node-sass'),
+        rExt: '.css',
+        useHash:true
+    })
+    .match('*.{js,css,png}',{
+        useHash:true
+    })
+    .match('/static/js/*.js',{
+        isMod:true
+    })
+    .hook('commonjs')
+    .match('::package',{
+        postpackager:fis.plugin('loader',{
+            resourceType: 'mod',
+            useInlineMap: true,
+            allInOne: {
+                includeAsyncs: true,
+                sourceMap: true,
+                useTrack: false
+            }
+        }),
+        packager:fis.plugin('map')
+    })
+
+
+// 清除其他配置，只保留如下配置
+fis.media('prod')
+    .match('*.js', {
+        // fis-optimizer-uglify-js 插件进行压缩，已内置
+        optimizer: fis.plugin('uglify-js')
+    })
+    .match('*.css',{
+        // fis-optimizer-clean-css 插件进行压缩，已内置
+        optimizer: fis.plugin('clean-css')
+    })
+    .match('*.png',{
+        // fis-optimizer-png-compressor 插件进行压缩，已内置
+        optimizer: fis.plugin('png-compressor')
     });
