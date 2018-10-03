4e108152788480a6cdff391a2946f81015a073ea
1052145436@qq.com <1052145436@qq.com>
2018-10-03 16:48:00 +0800

    【修改】单页逻辑

---
+++ static/js/order.js
@@ -0,0 +1,21 @@
+/*
+* @Author: Marte
+* @Date:   2016-02-03 14:49:02
+* @Last Modified by:   Marte
+* @Last Modified time: 2016-08-11 16:54:54
+*/
+var orderObj = {
+    init:function(){
+        require(['./jquery'],function($){
+            // 函数功能定义
+            var commonObj = {
+                initUI:function(){
+
+                }
+            }
+            // 执行函数
+            commonObj.initUI();
+        });
+    }
+}
+exports.orderObj = orderObj;
