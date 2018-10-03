c3c294e12f26583a5411145a29c4c546e0547cb2
1052145436@qq.com <1052145436@qq.com>
2018-10-03 14:43:33 +0800

    【修改】单页逻辑

--- static/js/address.js
+++ static/js/address.js
@@ -4,40 +4,13 @@
 * @Last Modified by:   Marte
 * @Last Modified time: 2016-08-11 16:54:54
 */
-var checkoutObj = {
+var addressObj = {
     init:function(){
         require(['./jquery'],function($){
             // 函数功能定义
             var commonObj = {
                 initUI:function(){
-                    // 所购买的商品列表
-                    var isShowGoodsList = false;
-                    $('#checkout-pro-list').click(function(){
-                        isShowGoodsList = !isShowGoodsList;
-                        if(isShowGoodsList == true){
-                            $('#checkout-proList-ul').stop().slideDown();
-                            $('#checkout-show-plBtn>span').html('收缩');
-                            $('#checkout-show-plBtn>i').addClass('rotateIcon');
-                        }else{
-                            $('#checkout-proList-ul').stop().slideUp();
-                            $('#checkout-show-plBtn>span').html('展开');
-                            $('#checkout-show-plBtn>i').removeClass('rotateIcon');
-                        }
-                    });
 
-                    // 添加更多商品
-                    $('#checkout-coupon-tip').click(function(){
-                        $('#checkout-more-goods').show();
-                        $('body').css({
-                            'overflow-y':'hidden'
-                        });
-                    });
-                    $('.checkout-more-goods-closeBtn').click(function(){
-                        $('#checkout-more-goods').hide();
-                        $('body').css({
-                            'overflow-y':'auto'
-                        });
-                    });
                 }
             }
             // 执行函数
@@ -45,4 +18,4 @@ var checkoutObj = {
         });
     }
 }
-exports.checkoutObj = checkoutObj;
+exports.addressObj = addressObj;
